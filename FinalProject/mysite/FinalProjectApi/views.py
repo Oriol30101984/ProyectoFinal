6fc1392be2c08fe6e30e55e689aef327b6b79868
Oriol <oriol.luis.boiria@gmail.com>
2022-09-08 18:15:13 +0200

    poner las listas en las views

--- FinalProject/mysite/FinalProjectApi/views.py
+++ FinalProject/mysite/FinalProjectApi/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+class GameViewSet():
+    queryset = Game.objects.all().order_by('name')
+    serializer_class = GameSerializer
+
+
+class PartyViewSet():
+    queryset = Party.objects.all().order_by('name')
+    serializer_class = PartySerializer
+
+
+class PartyMessageViewSet():
+    queryset = PartyMessage.objects.all().order_by('creation_date')
+    serializer_class = PartyMessageSerializer
