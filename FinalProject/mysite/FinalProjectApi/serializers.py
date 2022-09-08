25a2bb78a86a9f4bf2cb8900eca037d80dbd7f7d
Oriol <oriol.luis.boiria@gmail.com>
2022-09-08 19:40:06 +0200

    creación del resto de modelos y migración

--- FinalProject/mysite/FinalProjectApi/serializers.py
+++ FinalProject/mysite/FinalProjectApi/serializers.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 from .models import *
 
 
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+class GameSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Game
         fields = ('id', 'name', 'genre', 'launching_date')
 
+
+class PartySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Party
+        fields = ('name', 'game', 'creation_date')
+
+
+class PartyMessageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PartyMessage
+        fields = ('user', 'content', 'deleted', 'creation_date', 'updated_date')
